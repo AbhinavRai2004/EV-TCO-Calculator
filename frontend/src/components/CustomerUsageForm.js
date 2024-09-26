ac576ab0ecfef43e588cc709e9b582c823ee9015
Abhinav <raiabhinav.in@gmail.com>
2024-09-27 01:25:55 +0530

    CustomerUsage component added

---
+++ frontend/src/components/CustomerUsageForm.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import '../css/Form.css';
+
+const CustomerUsageForm = ({ customerUsage, setCustomerUsage }) => {
+  const handleInputChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const inputValue = type === 'checkbox' ? checked : value;
+    console.log(`Customer Usage Form - ${name}: ${inputValue}`);
+    setCustomerUsage({ ...customerUsage, [name]: inputValue });
+  };
+
+  return (
+    <div className="form-container">
+      <h2 className="form-heading">Customer Usage</h2>
+      <div className="form-group">
+        <label className="form-label">Monthly Km Driven</label>
+        <input
+          type="number"
+          className="form-input"
+          placeholder="Monthly Km Driven"
+          name="monthlyKm"
+          value={customerUsage.monthlyKm}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="form-group">
+        <label className="form-label">Calculation Duration (Years)</label>
+        <input
+          type="number"
+          className="form-input"
+          placeholder="Calculation Duration (Years)"
+          name="years"
+          value={customerUsage.years}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="form-group">
+        <label className="checkbox-label">
+          <input
+            type="checkbox"
+            name="considerBatteryReplacement"
+            checked={customerUsage.considerBatteryReplacement}
+            onChange={handleInputChange}
+          />
+          Consider Battery Replacement
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default CustomerUsageForm;
